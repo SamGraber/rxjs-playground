f1b7d96298550810f83f18c09427417ff9aa46d8
SamGraber <authorsamgraber@gmail.com>
2016-09-21 16:18:50 -0400

    Add operators
    Add zip, of, from, and do operators for the counter

--- source/rxjs-operators.ts
+++ source/rxjs-operators.ts
@@ -1,7 +1,11 @@
 import 'rxjs/add/observable/interval';
+import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/zip';
+import 'rxjs/add/observable/of';
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/take';
 import 'rxjs/add/operator/startWith';
 import 'rxjs/add/operator/scan';
 import 'rxjs/add/operator/last';
+import 'rxjs/add/operator/do';
