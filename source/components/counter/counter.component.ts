b00c9f3f29834f13b4fe93ed35398b047e722f51
SamGraber <authorsamgraber@gmail.com>
2016-09-21 16:17:04 -0400

    Simplify the counter using zip
    We can zip the interval together with an array of the actual values that we want and simply use it for timing. This is easier than trying to take the interval value and turn it into what we want.

--- source/components/counter/counter.component.ts
+++ source/components/counter/counter.component.ts
@@ -11,11 +11,11 @@ export class CounterComponent {
 	finished: boolean;
 
 	trigger(): void {
-		Observable.interval(1000)
-			.map(x => x + 1)
-			.startWith(0)
-			.take(11)
-			.map(x => 10 - x)
+		Observable.zip(
+				Observable.from([10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]),
+				Observable.interval(1000).startWith(0).take(11)
+			)
+			.map(x => x[0])
 			.subscribe(
 				x => this.val = x,
 				null,
