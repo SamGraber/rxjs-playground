d1733c6697df3507f268c37001d7f25a430fba50
SamGraber <authorsamgraber@gmail.com>
2016-09-21 12:12:37 -0400

    Added rxjs operators

---
+++ source/rxjs-operators.ts
@@ -0,0 +1,7 @@
+import 'rxjs/add/observable/interval';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/scan';
+import 'rxjs/add/operator/last';
