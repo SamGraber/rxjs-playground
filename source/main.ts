2cd9c74a46064f0881db65f37d722bdc2e79001b
SamGraber <authorsamgraber@gmail.com>
2016-09-21 12:12:59 -0400

    Main application

---
+++ source/main.ts
@@ -0,0 +1,7 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app.module';
+import './rxjs-operators';
+
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
